3968bad9d4e511084b98f05ecb13d904a43cfa7b
Ben <bencwallace@gmail.com>
2019-03-29 14:40:00 +0100

    added bridge initializer

--- pivot.jl
+++ pivot.jl
@@ -2,12 +2,57 @@ using Random
 include("rot.jl")
 
 
-function line(steps, dim=2)
-	e1 = zeros(Int, dim)
-	e1[1] = 1
+# ------------------------- Initializers ------------------------- #
 
-	return [i * e1 for i in 0:steps]
+
+function basis(i, dim=2)
+	e = zeros(Int, dim)
+	e[i] = 1
+	return e
 end
+
+
+function line(steps, dim=2)
+	return [i * basis(1, dim) for i in 0:steps]
+end
+
+
+function stairs(steps, dim=2)
+	horizontal = [Int(ceil(i / 2)) * basis(1, dim) for i=0:steps]
+	vertical = [Int(floor(i / 2)) * basis(2, dim) for i=0:steps]
+
+	return horizontal + vertical
+end
+
+
+function bridge(steps, dim=2)
+	e = [basis(i, dim) for i=1:dim]
+	options = [e..., [-item for item in e[2:dim]]...]
+
+	summands = Array{Array{Int, 1}}(undef, steps+1)
+	summands[1] = zeros(Int, dim)
+	for i=2:steps+1
+		valid = false
+		candidate = nothing
+		while !valid
+			candidate = rand(options)
+			if candidate == e[1] || candidate != -summands[i-1]
+				valid = true
+			end
+		end
+		summands[i] = candidate
+	end
+
+	output = Array{Array{Int, 1}}(undef, steps+1)
+	output[1] = summands[1]
+	for i=2:steps+1
+		output[i] = summands[i] + output[i-1]
+	end
+	return output
+end
+
+
+# ------------------------ Pivot algorithm ------------------------ #
 
 
 function pivot(polymer, step, Rot)
@@ -59,7 +104,7 @@ function mix(polymer, iter, callbacks=[], seed=nothing)
 	for i in 0:iter-1
 		# Diagnostics
 		if i % interval == 0
-			print("Iteration $i\t")
+			print("Iteration $i\n")
 			if !isempty(callbacks)
 				print("Callbacks: ")
 				for item in callbacks
